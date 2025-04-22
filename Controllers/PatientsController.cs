c38904d4de1544aa7827a50f1a182774a157054d
MynameisBI <56908557+MynameisBI@users.noreply.github.com>
2025-04-22 10:00:31 +0700

    Add register patient page

---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,54 @@
+﻿using Internship.Data;
+using Internship.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Internship.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HospitalDbContext _context;
+
+        public PatientsController(ILogger<HomeController> logger, HospitalDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var patient = await _context.Patients.ToListAsync();
+            return View(patient);
+        }
+
+        public IActionResult Register()
+        {
+            ViewBag.Cities = _context.Cities.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([Bind("PatientId", "PhoneNumber", "Name")] Patient patient)
+        {
+            patient.DateOfBirth = DateOnly.FromDateTime(DateTime.Now);
+            patient.Email = "hallo@hallo.com";
+            patient.CityId = 1;
+            patient.DistrictId = 1;
+            patient.WardId = 1;
+            patient.NumberAndRoad = "123 Main St";
+            patient.IsMale = true;
+            patient.NationalityId = 1;
+            patient.EthnicityId = 1;
+            patient.ProfessionId = 1;
+            if (ModelState.IsValid)
+            {
+                _context.Add(patient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+    }
+}
